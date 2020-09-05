6c01937afc7ee3856874ff650d896b98159bba72
CRAZY HACKER <52315456+PraveenKavindu@users.noreply.github.com>
2020-09-05 20:00:12 +0530

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -45,8 +45,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"Crazy Hacker BOT Provided by {DEFAULTUSER}\
-                          \nCrazy Hacker Helper to reveal all the plugin names\
+            help_string = f"Userbot Helper.. Provided by {DEFAULTUSER}\
+                          \nUserbot Helper to reveal all the plugin names\
                           \n__Do__ `.help` __plugin_name for commands, in case popup doesn't appear.__\
                           \nDo `.info` plugin_name for usage"
             results = await bot.inline_query(  # pylint:disable=E0602
@@ -70,7 +70,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            string = f"**Crazy Hacker BOT Provided by {DEFAULTUSER}\nCrazy Hacker Helper to reveal all the plugin names\n\n**Do `.help` plugin_name for commands\nDo `.info` plugin_name for usage\n\n"
+            string = f"**Userbot Helper.. Provided by {DEFAULTUSER}\nUserbot Helper to reveal all the plugin names\n\n**Do `.help` plugin_name for commands\nDo `.info` plugin_name for usage\n\n"
             for i in sorted(CMD_LIST):
                 string += "◆`" + str(i)
                 string += "`   "
