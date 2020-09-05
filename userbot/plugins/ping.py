09af22bed4ce1cd51e0b599b448eb792cc2003f8
CRAZY HACKER <52315456+PraveenKavindu@users.noreply.github.com>
2020-09-05 12:00:19 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -10,7 +10,7 @@ async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    event = await edit_or_reply(event ,"Pong!")
+    event = await edit_or_reply(event ," ◦•●◉✿ Pong ✿◉●•◦")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await event.edit("Pong!\n`{}`".format(ms))
