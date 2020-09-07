f6db4ec5e18eb2c23367adae0902f23172cc896f
CRAZY HACKER <52315456+PraveenKavindu@users.noreply.github.com>
2020-09-07 16:32:08 +0530

    Update Crazy Songs.py

--- userbot/plugins/Crazy Songs.py
+++ userbot/plugins/Crazy Songs.py
@@ -1,19 +1,19 @@
-""" Spotify / Deezer downloader plugin by @anubisxx | Syntax: .craze link"""
+""" Spotify / Deezer downloader plugin by @anubisxx | Syntax: .csc link"""
 import asyncio
 from telethon.errors.rpcerrorlist import YouBlockedUserError, UserAlreadyParticipantError
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("craze ?(.*)"))
+@borg.on(admin_cmd("csc ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
     d_link = event.pattern_match.group(1)
     if ".com" not in d_link:
-        await event.edit("` I need a link to download something pro.`")
+        await event.edit("` I need a link to download something pro.`**(._.)**")
     else:
-        await event.edit("🎶** Crazy Download! **🎶")
+        await event.edit("🎶**Initiating Download!**🎶")
 
     async with borg.conversation("@DeezerMusicBot") as conv:
         try:
@@ -25,11 +25,10 @@ async def _(event):
                 await asyncio.sleep(0.00000069420)
             await conv.send_message(d_link)
             details = await conv.get_response()
-            songh = await conv.get_response()
             await borg.send_message(event.chat_id, details)
-            await conv.get_response()
-            songh = await conv.get_response()
-            await borg.send_file(event.chat_id, songh, caption="🔆**Here's the requested song!**🔆")
+            res = await conv.get_response()
+            await borg.send_message(event.chat_id, res)
+            await borg.send_file(event.chat_id, await conv.get_response(), caption="🔆**Here's the requested song!**🔆")
             await event.delete()
         except YouBlockedUserError:
             await event.edit("**Error:** `unblock` @DeezerMusicBot `and retry!`")
